6daee3486f2ccbe64b4f4dd24de5dfa53bf82b8c
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-12-22 14:05:50 +0100

    Add models: ServiceType / Service

--- web_content/models.py
+++ web_content/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class ServiceType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    service_type = models.ForeignKey(
+        ServiceType, on_delete=models.CASCADE, related_name="services"
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
